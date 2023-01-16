2a3d889536f7fe6ba3d508b90a5f44c351892189
emartinez <enri.fc.98@gmail.com>
2023-01-16 11:17:02 +0100

    FIX: dont parse negative args

--- philo/src/parser.c
+++ philo/src/parser.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 10:20:58 by franmart          #+#    #+#             */
-/*   Updated: 2023/01/16 10:03:05 by franmart         ###   ########.fr       */
+/*   Updated: 2023/01/16 11:16:15 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,18 @@
 void	parse_args(int argc, char **argv, t_args *args)
 {
 	args->n_philos = secure_atoi(argv[1]);
+	if (args->n_philos < 1)
+		exit(1);
 	args->die_time = secure_atoi(argv[2]);
 	args->eat_time = secure_atoi(argv[3]);
 	args->sleep_time = secure_atoi(argv[4]);
 	args->eat_limit = -1;
+	args->dead = 0;
 	if (argc == 6)
+	{
 		args->eat_limit = secure_atoi(argv[5]);
+		if (args->eat_limit < 1)
+			exit(1);
+	}
 	//printf("%d %d %d %d %d\n", args->n_philos, args->die_time, args->eat_time, args->sleep_time, args->eat_limit);
 }
